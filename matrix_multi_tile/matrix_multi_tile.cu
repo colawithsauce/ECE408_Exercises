b95338a7b506537f649f6d4a388a23e91bdfc85e
colawithsauce <cola_with_sauce@foxmail.com>
2023-04-28 11:25:00 +0800

    update

--- matrix_multi_tile/matrix_multi_tile.cu
+++ matrix_multi_tile/matrix_multi_tile.cu
@@ -2,8 +2,6 @@
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 
-#include "../cuda_alias.h"
-
 #define TILE_WIDTH 16
 
 // @param:
@@ -13,29 +11,44 @@
 //   And we assume that blockDim.x == blocDim.y && blockDim.x == TILE_WIDTH
 template <typename T>
 __global__ void
-matrix_multi_tile_kernel(const T* A_d, const T* B_d, T* C_d, int M, int N, int S, int sza, int width)
+matrix_multi_tile_kernel(const T* A_d, const T* B_d, T* C_d, int M, int N, int S, int nSZa, int nBlkWidth)
 {
     // Initialize space for tiling multiplication
     extern __shared__ T Ads_Bds[]; // Defined in the kernel arguments
     T* Ads = Ads_Bds;
-    T* Bds = Ads_Bds + sza;
+    T* Bds = Ads_Bds + nSZa;
 
     const int bx = blockIdx.x;
     const int by = blockIdx.y;
     const int tx = threadIdx.x;
     const int ty = threadIdx.y;
 
-    int Row = width * blockIdx.y + threadIdx.y;
-    int Col = width * blockIdx.x + threadIdx.x;
+    int Row = nBlkWidth * blockIdx.y + threadIdx.y;
+    int Col = nBlkWidth * blockIdx.x + threadIdx.x;
 
     // do matrix multiplication
     T Cvalue = 0;
-    for (int ph = 0; ph < ceil(max(M / (float)width, N / (float)width)); ph++) {
-        // Load from A and B
-        if (Row < width && (ph * width + tx)) {
+    for (int ph = 0; ph < ceil(max((float)M / nBlkWidth, (float)N / nBlkWidth)); ph++) {
+        if (Row < M && (ph * nBlkWidth + tx) < S) {
+            Ads[ty * nBlkWidth + tx] = A_d[Row * S + (ph * nBlkWidth + tx)];
+        } else {
+            Ads[ty * nBlkWidth + tx] = 0;
         }
 
-        // Accumulate this result.
+        if (Col < N && (ph * nBlkWidth + ty) < S) {
+            Bds[ty * nBlkWidth + tx] = B_d[(ph * nBlkWidth + ty) * N + Col];
+        } else {
+            Bds[ty * nBlkWidth + tx] = 0;
+        }
+
+        __syncthreads();
+
+        // Accumulate the result in this phase
+        for (int i = 0; i != nBlkWidth; i++) {
+            Cvalue += Ads[ty * nBlkWidth + i] * Bds[i * nBlkWidth + tx];
+        }
+
+        __syncthreads();
     }
 
     C_d[(Row * N) + Col] = Cvalue;
